34f6256a73879cc289a60af7e36557c23a9b38f7
Scarlett <scarlett@electronika.xyz>
2021-12-03 20:56:14 -0500

    wrong import

--- packutil.py
+++ packutil.py
@@ -1,16 +1,16 @@
 # packutil authored by team electronika
 import PySimpleGUI as sg
-from black import sys
 from src.fileOp import GUIFileCopy
 import src.packInitSetup as iSetup
 import src.web as web
 import os
+import sys
 from zipfile import ZipFile
 
 sg.theme("DarkPurple1")
 
 
-def ControlledExit(reason=None): #exit while notifying user
+def ControlledExit(reason=None):  # exit while notifying user
     if reason == None:
         sg.Window(
             "Packutil is exiting",
@@ -31,7 +31,9 @@ def ControlledExit(reason=None): #exit while notifying user
     sys.exit(1)
 
 
-if not os.path.exists("textures"): #checks if textures exists- if not download and extract them
+if not os.path.exists(
+    "textures"
+):  # checks if textures exists- if not download and extract them
     sg.popup(
         "Packutil will now download the default textures for modification. This could take a little while."
     )
@@ -42,19 +44,21 @@ if not os.path.exists("textures"): #checks if textures exists- if not download a
     with ZipFile("textures/textures.zip", "r") as zipFile:
         total = len(zipFile.namelist())
         updateProg = 1
-        for file in zipFile.namelist(): #namelist is a list of every file in the zip
+        for file in zipFile.namelist():  # namelist is a list of every file in the zip
             updateProg += 1
-            if updateProg == 500: #only update meter every 500 files because there's 40k of them
+            if (
+                updateProg == 500
+            ):  # only update meter every 500 files because there's 40k of them
                 sg.one_line_progress_meter(
                     "Texture Extract",
-                    zipFile.namelist().index(file), #find current point in extraction
+                    zipFile.namelist().index(file),  # find current point in extraction
                     total,
                     "Extracting textures, please be patient",
                     no_button=True,
                 )
                 updateProg = 1
             zipFile.extract(file, "textures")
-        sg.one_line_progress_meter( #sometimes the bar doesn't get fully finished, this makes sure that happens
+        sg.one_line_progress_meter(  # sometimes the bar doesn't get fully finished, this makes sure that happens
             "Texture Extract",
             total,
             total,
@@ -81,12 +85,16 @@ if folder == None:
     ControlledExit("Folder choosing was cancelled.")
 
 if event == "Existing":
-    if not os.path.exists(f"{folder}/pack.mcmeta"): #see if we've already init'd
+    if not os.path.exists(f"{folder}/pack.mcmeta"):  # see if we've already init'd
         sg.popup(
             "This folder isn't set up for a Resource Pack. Try using the 'New' option."
         )
         ControlledExit()
 else:
-    version = iSetup.InitSetup(folder) #init setup returns a version field so we can copy the default ones
-    sg.popup(f'Packutil will now copy the default texures for {version} to the project folder.')
+    version = iSetup.InitSetup(
+        folder
+    )  # init setup returns a version field so we can copy the default ones
+    sg.popup(
+        f"Packutil will now copy the default texures for {version} to the project folder."
+    )
     GUIFileCopy(f"textures/{version}/assets", f"{folder}/assets")
